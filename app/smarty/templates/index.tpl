e3472488a12dbab6d1357bd38de3542c9822ab81
Nurbol Makashov <makashov92@gmail.com>
2026-06-28 13:29:44 +0500

    Setup SCSS

--- app/smarty/templates/index.tpl
+++ app/smarty/templates/index.tpl
@@ -1,1 +1,17 @@
-Hello {$user}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Main Page</title>
+    <style>
+        {$css}
+    </style>
+</head>
+<body>
+
+<nav>
+    <span>Main Page</span>
+</nav>
+
+</body>
+</html>
--- public/index.php
+++ public/index.php
@@ -3,8 +3,14 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use App\Smarty\Smarty;
+use ScssPhp\ScssPhp\Compiler;
+
+$compiler = new Compiler();
+$compiler->setImportPaths('../app/scss/');
+
+$css = $compiler->compileString('@import "style.scss"')->getCss();
 
 $smarty = new Smarty();
 
-$smarty->assign('user', 'Nurbol');
+$smarty->assign('css', $css);
 $smarty->display('index.tpl');
